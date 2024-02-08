b455ceb60082919a348febf51277400e5663c40b
yangyanbo <yangyanbo@fuzhi.ai>
2024-02-08 09:52:44 +0800

    feat: 调度流程->lane模型

---
+++ packages/react-reconciler/src/fiberLanes.ts
@@ -0,0 +1,14 @@
+export type Lane = number;
+export type Lanes = number;
+
+export const SyncLane = 0b0001;
+export const NoLane = 0b0000;
+export const NoLanes = 0b0000;
+
+export function mergeLanes(a: Lanes, b: Lanes): Lanes {
+  return a | b;
+}
+
+export function requestUpdateLane(): Lane {
+  return SyncLane; // 同步更新
+}
--- packages/react-reconciler/src/workLoop.ts
+++ packages/react-reconciler/src/workLoop.ts
@@ -3,6 +3,7 @@ import { commitMutationEffect } from './commitWork';
 import { completeWork } from './completeWork';
 import { FiberNode, FiberRootNode, createWorkInProgress } from './fiber';
 import { MutationMask, NoFlags } from './fiberFlags';
+import { Lane, mergeLanes } from './fiberLanes';
 import { HostRoot } from './workTags';
 
 let workInProgress: FiberNode | null = null;
@@ -11,11 +12,16 @@ function prepareFreshStack(root: FiberRootNode) {
   workInProgress = createWorkInProgress(root.current, {});
 }
 
-export function scheduleUpdateOnFiber(fiber: FiberNode) {
+export function scheduleUpdateOnFiber(fiber: FiberNode, lane: Lane) {
   // TODO:调度更新
   // fiberRootNode
   const root = markUpdateFromFiberToRoot(fiber);
+  markRootUpdated(root, lane);
   renderRoot(root);
+}
+
+function markRootUpdated(root: FiberRootNode, lane: Lane) {
+  root.pendingLanes = mergeLanes(root.pendingLanes, lane);
 }
 
 // 向上遍历找到根节点
